774bc8c5bdefd34f23cd84daa8c9da87408f4ae6
Mohab Alnajjar <72289694+mo662008@users.noreply.github.com>
2022-11-12 17:33:15 +0200

    Update 43B.cpp

--- B/43B.cpp
+++ B/43B.cpp
@@ -32,3 +32,35 @@ int main() {
 	cout << "YES";
 	return 0;
 }
+
+
+
+
+#include <bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
+using namespace std;
+
+int main() {
+	fastio
+	unordered_map<char, int> m;
+	string s1, s2;
+	getline(cin, s1);
+	getline(cin, s2);
+	sort(s1.begin(), s1.end());
+	sort(s2.begin(), s2.end());
+	int p2(0);
+	while (s2[p2] == ' ') ++p2;
+	for (char c: s1) {
+		if (c == ' ') continue;
+		if (s2[p2] == c) {
+			++p2;
+		}
+	}
+	if (p2 == s2.size()) {
+		cout << "YES";
+	} else {
+		cout << "NO";
+	}
+	return 0;
+}
